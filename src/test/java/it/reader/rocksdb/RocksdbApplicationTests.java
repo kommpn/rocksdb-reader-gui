f60f40f8ee428402f9beb7e7eae995e75fcb4034
kommpn <kommpn@ther4d4r-2.local>
2023-11-14 23:46:09 +0100

    remove unused test

--- src/test/java/it/reader/rocksdb/RocksdbApplicationTests.java
+++ src/test/java/it/reader/rocksdb/RocksdbApplicationTests.java
@@ -2,7 +2,7 @@ package it.reader.rocksdb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class RocksdbApplicationTests {
 
@@ -10,4 +10,4 @@ class RocksdbApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
